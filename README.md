32556b83565e4ff01fec2b928553f34b672bfe26
peedroca <phm1080@gmail.com>
2023-05-22 07:24:33 -0300

    Added nifi process to load data from Exchange Rate API to MongoDB and MySQL

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# fiap_data_ingestion
-Project to learn how implement an data architecture, integration and ingestion
+# FIAP Data Ingestion
+
+Hello everyone!
+
+Let's create an amazing repository about data ingestion. I'll use Docker to set up our ecosystem. We need some databases to populate with sample data. We have chosen MySQL as our relational database management system (RDBMS), and Cassandra and MongoDB as our NoSQL databases.
+
+Here are some details about the use case: We are an e-commerce company specializing in shoe sales. We have recently acquired a small competitor e-commerce business and obtained their CSV files containing product and customer data.
+
+Now, we need to populate our databases with this data in order to extract and present it in dashboards. We will automate this process using Apache NiFi to create a data pipeline.
+
+Let's enhance the insights of SalesHub with data analysis!
+
+## Pre-requirements
+
+- Docker (used Docker Desktop 4.19.0 when I write this)
+
+## Diagram Entity Relationship
+
+![DER](/assets/SalesHubDER.drawio.png)
+
+## Setting up Staging Ambient
+
+On root folder run the code `docker-compose up -d` to create our ecosystem.
+
+We are equiped with some GUI to help us manage data.
+
+Graphical User Interface | Database | Address
+:-- | :--: | :--:
+Adminer | MySQL | http://localhost:8080
+Mongo Express | MongoDB | http://localhost:8081
+Cassandra Web | Cassandra | http://localhost:3000
+Apache NiFi | - | http://localhost:9090/nifi
+
+#### Cassandra WEB
+
+After generate the ecosystem, you need configure Cassandra Web: 
+Firstly get CassandraDB ipv4 with command `docker inspect cassandra`, then put the address ipv4 in variable *CASSANDRA_HOST_IPS* at `docker-compose.yml`. Finally, execute `docker rm cassandra-web & docker-compose up -d cassandra-web`.
+
+## Using NiFi to ingest data from APIs
+
+We'll use MongoDB to storage raw data from API calls, Update data about log transactions on Cassandra, and storage data about exchange rate USD to BRL and EUR to BRL on MySQL.
+
+Start preparing Apache NiFi container with mysql connector, to insert data on mysql. Use `docker cp utils/mysql-connector-j-8.0.33.jar nifi:/opt/nifi/nifi-current/lib`
+
+#### Process used to ingest data
+
+![NiFi](/assets/nifi_process.png)
+> ImportExchangeRate
+
+InvokeHTTP - Request data from API.
+PutMongoRecord - Save raw data on MongoDB.
+EvaluateJsonPath - Load attributes with Json data.
+PutSQL - Save attributes data on MySQL.
---
+++ scripts/MySQL/DML_SalesHub.sql
@@ -0,0 +1,246 @@
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Alessandra Francisca Cavalcanti', 'Rua Paraná', '85035010', 'alessandra-cavalcanti70@crbrandao.com.br', '(42) 98245-8529', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Carlos Eduardo Silva', 'Avenida Paulista', '01310100', 'carlos.silva@example.com', '(11) 98765-4321', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Mariana Rodrigues Santos', 'Rua das Flores', '70000000', 'mariana.santos@example.com', '(61) 99876-5432', '2023-05-18 19:30:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Ricardo Oliveira', 'Rua Bela Vista', '04000000', 'ricardo.oliveira@example.com', '(11) 98765-4321', '2023-05-18 19:40:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Fernanda Santos', 'Avenida Rio Branco', '20000000', 'fernanda.santos@example.com', '(21) 99876-5432', '2023-05-18 19:50:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Gustavo Lima', 'Rua das Oliveiras', '04000000', 'gustavo.lima@example.com', '(11) 98765-4321', '2023-05-18 20:00:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Juliana Almeida', 'Avenida Atlântica', '22000000', 'juliana.almeida@example.com', '(21) 99876-5432', '2023-05-18 20:10:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Pedro Sousa', 'Rua dos Pinheiros', '05422000', 'pedro.sousa@example.com', '(11) 98765-4321', '2023-05-18 20:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Carolina Fernandes', 'Avenida Brasil', '40000000', 'carolina.fernandes@example.com', '(21) 99876-5432', '2023-05-18 20:30:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES
+('Gustavo Sousa', 'Rua dos Pinheiros', '05422000', 'gustavo.sousa@example.com', '(11) 98765-4321', '2023-05-18 21:50:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Cristiane Carolina Patricia de Paula', 'Rua K', '11335317', 'cristiane.carolina.depaula@projetochama.com.br', '13999399882', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Benedita Marli Isabela Melo', 'Rua Boaventura Donato Velho', '88708669', 'benedita_marli_melo@ovi.com', '48994363256', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Fernando Pietro Moraes', 'Rua Angela Maria Santana Ribeiro', '49040350', 'fernando_pietro_moraes@vhbadvogados.com.br', '79989994968', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Joao Antonio Assuncao', 'Rua Monteiro Lobato', '85908020', 'jose_antonio_assuncao@plaman.com.br', '45987368512', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Marcos Breno Dias', 'Rua CenturiÃ£o', '68907290', 'marcosbrenodias@cassianoricardo.com.br', '96994048605', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Ruan Matheus Pietro Corte Real', 'Rua MaranhÃ£o', '57010170', 'ruan_cortereal@atualecomex.com.br', '82981493795', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Elias Luis Davi Cardoso', 'Rua JosÃ© EulÃ¡lio', '65051320', 'elias_luis_cardoso@gilconsultoria.com.br', '98983100335', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Mirella Sophia Marcela da Costa', 'Rua Cesar Sciullo', '99709058', 'mirellasophiadacosta@trietto.com.br', '54991837383', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Bernardo Pedro Henrique Nascimento', 'Rua Marta Santana', '24475390', 'bernardo.pedro.nascimento@tonyveiculos.com.br', '21983001726', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Customers (Name, Address, CEP, Email, Phone, RegisterDate, Active) VALUES 
+('Luiza Isabelle Bruna Figueiredo', 'Rua 2', '77424180', 'luiza.isabelle.figueiredo@gemail.com', '63993931016', '2023-05-18 19:20:00', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('2', 'Rua Cento e Quarenta e Dois', '53405817', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('15', 'Rua Ipiranga', '79106791', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('4', 'Beco Dona Matilde', '31050532', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('21', 'Rua Dois Mil e Seiscentos', '78075650', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('13', 'Rua Nova Escócia', '21830315', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('11', 'Rua Santa Cândida', '83829106', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('8', 'Rua Padroeiro São Benedito', '49071056', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('6', 'Rua 8', '75073138', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('17', 'Rua Manoel Timóteo dos Santos', '44003664', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('1', 'Rua Luiz Alves', '89211665', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('17', 'Rua Antonio Pinto', '24737505', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('12', 'Rua Francisco Ferreira', '69901328', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('15', 'Rua Pedro dos Santos', '94015540', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('14', 'Quadra T 33 Rua LO 21', '77063558', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('17', 'Travessa da Mata', '40221280', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('6', 'Rua São Severino dos Ramos', '59123550', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('10', 'Rua Voluntários da Pátria', '90030002', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('21', 'Rua Terezina', '76908418', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('17', 'Rua 11 de Janeiro', '41197080', '2023-05-20 12:03:46', 1);
+
+INSERT INTO Orders (CustomerID, Address, CEP, RegisterDate, Active) VALUES 
+('16', 'Rua Trinta e Sete', '83210346', '2023-05-20 12:03:46', 1);
+
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('29522198', 'URY Steel Necklace', 'Joyeria Milan', 'Microsoft', '"[green, purple]"', '"[large, small]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('11133136', '109F Solid Womens Tunic', 'Blue', 'Apple', '"[green, green]"', '"[large, medium]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('42439949', 'Eshoppee Scorpion Gold Finish Alloy Necklace', 'Allure Auto', 'Nike', '"[green, blue]"', '"[large, large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('31112395', 'Angel Glitter School Bag', 'Rapter', 'Samsung', '"[purple, red]"', '"[extra-large, large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('37473471', 'Speedwav 240411 Sun Shade For Honda Jazz', 'nan', 'Samsung', '"[purple, green]"', '"[medium, extra-large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('31058330', 'FabAlley Casual Sleeveless Solid Womens Top', 'Estella Fashion', 'Microsoft', '"[blue, red]"', '"[small, small]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('24257593', 'Clickroo Mens Pyjama', 'Foot Candy', 'Apple', '"[red, red]"', '"[large, medium]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('36513942', 'BlueStone The Triad Flourish Gold Diamond 18 K Ring', 'WearYourShine by PCJ', 'Apple', '"[blue, purple]"', '"[medium, small]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('17630608', 'Perucci PCC-502C Decker Analog Watch  - For Men', 'nan', 'Apple', '"[purple, green]"', '"[small, medium]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('37032439', 'Krizel Trendz Crepe Printed Salwar Suit Dupatta Material', 'Pink', 'Toyota', '"[red, green]"', '"[extra-large, large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('32630118', 'Pet Club51 PC237 L Pet Bed', 'Radiant Bay', 'Nike', '"[green, purple]"', '"[medium, small]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('88997342', 'Unique Designes Flip Cover for domo x3G tablet (7inch)', 'Voylla', 'Microsoft', '"[green, purple]"', '"[medium, medium]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('16597650', 'Amber Cotton Bath Towel', 'Karukraft', 'Samsung', '"[red, yellow]"', '"[medium, small]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('44341560', 'Foot Candy Women Heels', 'nan', 'Apple', '"[yellow, blue]"', '"[medium, small]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('41085992', 'Lajo Womens Salwar and Kurta Set', 'nan', 'Nike', '"[yellow, purple]"', '"[small, extra-large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('92175202', 'C&S Shopping Gallery Womens Leggings', 'nan', 'Nike', '"[green, purple]"', '"[small, extra-large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('16240308', 'JDX Alloy Necklace', 'next steps', 'Microsoft', '"[purple, blue]"', '"[small, extra-large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('38100388', 'Engage Fuzz and Urge Combo Set', 'AutoGarh', 'Apple', '"[blue, red]"', '"[small, medium]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('95181593', 'Veensh Red HARB Grinder Diameter 2.25 inch Plastic Hookah', 'nan', 'Microsoft', '"[blue, yellow]"', '"[extra-large, medium]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('11529348', 'Bodycare Boys Brief', 'Nutcase', 'Toyota', '"[blue, blue]"', '"[small, extra-large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO Products (Code, Name, Model, Manufacturer, Color, Size, RegisterDate, Active) VALUES
+('60918953', 'Unravel india Jodhpuri Peacock Multicolored Wine Bottle Holder Showpiece  -  33.02 cm', 'Autoplus', 'Nike', '"[yellow, green]"', '"[medium, large]"', '2023-05-20 13:00:34', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('3', '10', '276', '1.14', '314.64', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('15', '4', '826', '37.92', '31321.92', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('10', '6', '725', '37.1', '26897.5', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('17', '7', '171', '3.84', '656.64', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('14', '13', '864', '21.94', '18956.16', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('18', '3', '103', '20.32', '2092.96', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('20', '12', '83', '4.86', '403.38', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('12', '21', '88', '66.47', '5849.36', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('18', '11', '799', '0.59', '471.41', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('10', '10', '58', '4.71', '273.18', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('10', '18', '358', '8.75', '3132.5', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('9', '6', '449', '0.54', '242.46', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('4', '1', '74', '19.27', '1425.98', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('21', '18', '705', '35.0', '24675.0', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('5', '1', '989', '1.14', '1127.46', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('3', '10', '408', '6.05', '2468.4', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('13', '5', '776', '20.06', '15566.56', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('20', '7', '909', '8.15', '7408.35', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('18', '6', '584', '7.08', '4134.72', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('17', '10', '700', '7.48', '5236.0', '2023-05-20 12:45:15', 1);
+
+INSERT INTO OrderItems (OrderID, ProductID, Quantity, UnitValue, TotalValue, RegisterDate, Active) VALUES
+('10', '7', '487', '42.09', '20497.83', '2023-05-20 12:45:15', 1);
